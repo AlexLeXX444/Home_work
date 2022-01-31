469645dcdc116f4327e2c3e2b418922d3a7b158e
AlexLeXX444 <alexlexx444@yandex.ru>
2022-02-01 00:46:51 +0500

    решил задачу 2. Даны два числа. Показать большее и меньшее число

---
+++ Exam_0002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2)
+{
+    if (num1 > num2)
+        Console.WriteLine("Число " + num1 + " больше числа " + num2 + ".");
+    else
+        Console.WriteLine("Числа равны.");
+} else 
+    Console.WriteLine("Число " + num1 + " меньше числа " + num2 + ".");
