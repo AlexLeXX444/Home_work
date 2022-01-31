fe63e5277f3fff77915e8c69faa9065129d3b46c
AlexLeXX444 <alexlexx444@yandex.ru>
2022-02-01 00:36:58 +0500

    сделал задачу 1. По двум заданным числам проверять является ли первое квадратом второго

---
+++ Exam_0001/Program.cs
@@ -0,0 +1,12 @@
+﻿//  По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 / num2 == num2)
+    Console.WriteLine("Число " + num1 + " является квадратом числа " + num2 + ".");
+else 
+    Console.WriteLine("Число " + num1 + " НЕ является квадратом числа " + num2 + ".");
