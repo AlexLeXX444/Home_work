c20d0ae21b4a83d212ad7a3f7ab124aece093ca8
AlexLeXX444 <alexlexx444@yandex.ru>
2022-02-03 22:52:33 +0500

    решил задачу 7. Показать числа от -N до N

---
+++ Exam_0007/Program.cs
@@ -0,0 +1,39 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Программы выводит числа от -N до N.");
+Console.WriteLine("Будете вводить чило N самостоятельно? Введите `y` или 'yes' если согласны.");
+Console.WriteLine("Если нежелаете , то программа сгенерирует число в диапазоне 1...1000.");
+string? answer_user = Console.ReadLine();
+int num;
+
+// Генерируем рандомное число от 1 до 1000.
+static int Random_num ()
+{
+    Random rnd = new Random();
+    int num = rnd.Next(1,1000);
+
+    return num;
+}
+
+// Считываем ввод пользователя.
+static int Enter_num ()
+{
+    Console.WriteLine("Введите число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    return num;
+}
+
+// Заполняем значения для дальнейших расчётов.
+if (answer_user == "y" || answer_user == "yes") 
+{
+    num = Enter_num(); 
+} else
+{
+    num = Random_num();
+    Console.WriteLine("Сгенерировано число: " + num + ".");
+}
+
+// Рассчитываем и выводим данные в консоль.
+for (int i = (num * (-1)); i <=num; i++)
+    Console.Write(i + "  ");
