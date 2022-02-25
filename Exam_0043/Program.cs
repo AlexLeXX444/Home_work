fa22f89274cac24669eb4d8cb2532b4fdccf2072
AlexLeXX444 <alexlexx444@yandex.ru>
2022-02-26 01:23:19 +0500

    решил задачи 43 и 44

---
+++ Exam_0043/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+Console.WriteLine("Введите число:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+int[] answerMass = new int[FindTimesDelTwo(userNum)];
+
+// Рачссчитываем, сколько раз число делится на 2.
+int FindTimesDelTwo (int n)
+{
+    int delTwo = 0;
+    while (n > 0)
+    {
+        n = n / 2;
+        delTwo++;
+    }    
+    return delTwo;
+}
+
+// Заполняем массив ответами.
+int[] ConvertTwiceNum (int n, int[] mass)
+{
+    for (int i = FindTimesDelTwo(n) - 1; i >= 0; i--)
+    {
+        mass[i] = n % 2;
+        n = n / 2;
+    }
+    return mass;
+}
+
+// Выводим массив в консоль одной единой строкой.
+void PrintMassString (int[] mass)
+{
+    Console.WriteLine("Получили двоичное число:");
+    for (int i = 0; i < mass.Length; i++)
+        Console.Write(mass[i]);    
+}
+
+// Собираем всё воедино.
+ConvertTwiceNum(userNum, answerMass);
+PrintMassString(answerMass);
---
+++ Exam_0044/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением 
+// y1 = k1 * x + b1, y2 = k2 * x + b2, 
+// b1 k1 и b2 и k2 заданы.
+
+Console.WriteLine("Введите угловой коэффициэнт первой прямой (к1).");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку первой прямой (b1).");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите угловой коэффициэнт второй прямой (к2).");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку второй прямой (b2).");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 != k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    Console.WriteLine("Точка пересечения прямых расположена в координатах:");
+    Console.WriteLine("Х ::: " + x);
+    Console.WriteLine("Y ::: " + (k1 * x + b1));
+} else
+    Console.WriteLine("Прямые параллельны.");
