dace431c536e356c42c6cd16600533dea8d8c014
AlexLeXX444 <alexlexx444@yandex.ru>
2022-02-27 12:54:34 +0500

    решил задачу 48

---
+++ Exam_0048/Program.cs
@@ -0,0 +1,50 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами.
+
+Console.WriteLine("Введите количество строк массива. Число должно быть положительное и больше нуля.");
+int str = Convert.ToInt32(Console.ReadLine());
+UserEnterNum(str);
+Console.WriteLine("Введите количество столбцов массива. Число должно быть положительное и больше нуля.");
+int stolb = Convert.ToInt32(Console.ReadLine());
+UserEnterNum(stolb);
+
+int[,] mass = new int[str,stolb];
+
+// Проверяем ввод пользователя.
+int UserEnterNum (int n)
+{
+    while (n < 1)
+    {
+        Console.WriteLine("Введенное число не соответствует требованиям. Повторите ввод.");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    return n;
+}
+
+// Заполняем массив рандомными целыми числами.
+int[,] RandomNumArray (int[,] mass)
+{
+    for (int i=0; i<str; i++)
+    {
+        for (int j=0; j<stolb; j++)
+            mass[i,j] = new Random().Next(1,1000);
+    }
+    return mass;
+}
+
+// Выводим многомерный массив на консоль.
+void PrintMultiArray (int[,] mass)
+{
+    Console.WriteLine("Получили массив:");
+
+    for (int i=0; i<str; i++)
+    {
+        Console.Write("| ");
+        for (int j=0; j<stolb; j++)
+            Console.Write(mass[i,j] + " | ");
+        Console.WriteLine();
+    }
+}
+
+RandomNumArray(mass);
+Console.Clear();
+PrintMultiArray(mass);
