c2cb16a04dd115e73db2c6cd8de1584b1bc52e18
AlexLeXX444 <alexlexx444@yandex.ru>
2022-03-28 23:48:01 +0500

    решил задачу 66

---
+++ Exam_0066/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать натуральные числа от 1 до N, N задано.
+
+Console.WriteLine("Введите число. Оно должно быть больше 1.");
+int num = Convert.ToInt32(Console.ReadLine());
+num = CheckUserEntries(num);
+
+// Проверяем ввод пользователя.
+int CheckUserEntries (int num)
+{
+    while (num < 2)
+    {
+        Console.WriteLine("Некорретный ввод. Повторите.");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+// Вывод на консоль нужных чисел.
+void ShowNumbers (int num)
+{
+    if (num < 1)
+        return;
+
+    ShowNumbers(num - 1);
+    Console.Write($"{num, 3} ");
+}
+
+ShowNumbers(num);
