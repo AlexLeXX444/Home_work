7f37a2ecd2e060c17a5b2c791b3698c263152a80
AlexLeXX444 <alexlexx444@yandex.ru>
2022-03-29 01:22:46 +0500

    решил задачу 70

---
+++ Exam_0070/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму цифр числа
+
+Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+    number = number * (-1);
+
+// Находим сумму числа.
+int SummOfNumber (int num)
+{
+    if (num < 1)
+        return 0;
+    
+    return SummOfNumber(num/10) + num%10;
+}
+
+Console.WriteLine("Получили сумму цифр заданного числа: " + SummOfNumber(number));
