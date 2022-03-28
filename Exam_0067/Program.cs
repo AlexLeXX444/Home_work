fe15793c984fd4ba1d20b310b227c9d550a2d7d9
AlexLeXX444 <alexlexx444@yandex.ru>
2022-03-28 23:57:04 +0500

    решил задачу 67

---
+++ Exam_0067/Program.cs
@@ -0,0 +1,40 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+
+Console.WriteLine("Введите число. Оно может положительным или отрицательным, НО не должно равняться единице 1.");
+int num = Convert.ToInt32(Console.ReadLine());
+num = CheckUserEntries(num);
+
+// Проверяем ввод пользователя.
+int CheckUserEntries (int num)
+{
+    while (num == 1)
+    {
+        Console.WriteLine("Некорретный ввод. Повторите.");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+// Вывод на консоль положительных чисел.
+void ShowNumbersPlus (int num)
+{
+    if (num < 1)
+        return;
+
+    Console.Write($"{num, 3} ");
+    ShowNumbersPlus(num - 1);
+}
+
+// Вывод на консоль положительных чисел.
+void ShowNumbersMinus (int num)
+{
+    if (num > 1)
+        return;
+
+    Console.Write($"{num, 3} ");
+    ShowNumbersMinus(num + 1);
+}
+
+Console.Clear();
+ShowNumbersPlus(num);
+ShowNumbersMinus(num);
