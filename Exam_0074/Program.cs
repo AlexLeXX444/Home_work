25f67629a9c0da56d9e530e4c9af68cc68b1f38c
AlexLeXX444 <alexlexx444@yandex.ru>
2022-03-30 09:19:41 +0500

    решил задачу 74

---
+++ Exam_0074/Program.cs
@@ -0,0 +1,25 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+
+Console.WriteLine("Введите число N:");
+int num = int.Parse(Console.ReadLine());
+
+String symbols = "аисв";
+
+int n = 1;
+
+void FindWords(string alfavit, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new string(word)}"); 
+        return;
+    }
+
+    for (int i = 0; i < alfavit.Length; i++)
+    {
+        word[length] = alfavit[i];
+        FindWords(alfavit, word, length + 1);
+    }
+}
+
+FindWords(symbols, new char[num]);
